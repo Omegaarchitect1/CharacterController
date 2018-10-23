9f825dea58e9fb01f6bf0b98e82eedb33e7ccd2e
Mitchell Quigg <Mitchell.Quigg@iam.local>
2018-10-23 15:16:32 -0500

    Updated Scripts

--- Assets/Scripts/BunnyControl.cs
+++ Assets/Scripts/BunnyControl.cs
@@ -6,19 +6,20 @@ public class BunnyControl : MonoBehaviour {
 
 
     public float maxspeed = 10f;
-    bool facingright = true; 
+    bool facingright = true;
+    Rigidbody2D rigidbody2D; 
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        rigidbody2D = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
         float move = Input.GetAxis("Horizontal");
-        Rigidbody2D.velocity = new Vector2(move * maxspeed, Rigidbody2D.velocity.y);
+        rigidbody2D.velocity = new Vector2(move * maxspeed, rigidbody2D.velocity.y);
 	}
 
     void Flip()
