24123b79b929600bfc156130e7a6e1bebe0b9015
Mitchell Quigg <Mitchell.Quigg@iam.local>
2018-10-23 15:13:53 -0500

    Started Player control script

---
+++ Assets/Scripts/BunnyControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyControl : MonoBehaviour {
+
+
+    public float maxspeed = 10f;
+    bool facingright = true; 
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float move = Input.GetAxis("Horizontal");
+        Rigidbody2D.velocity = new Vector2(move * maxspeed, Rigidbody2D.velocity.y);
+	}
+
+    void Flip()
+    {
+
+    }
+}
