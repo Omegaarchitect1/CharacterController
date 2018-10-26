7b26dc48d2fd918545bb3a4159679f4ca994517b
Mitchell Quigg <Mitchell.Quigg@iam.local>
2018-10-26 13:33:09 -0500

    Set up runnng animations

--- Assets/Scripts/BunnyControl.cs
+++ Assets/Scripts/BunnyControl.cs
@@ -7,23 +7,41 @@ public class BunnyControl : MonoBehaviour {
 
     public float maxspeed = 10f;
     bool facingright = true;
-    Rigidbody2D rigidbody2D; 
+    Rigidbody2D rigidbody2D;
+
+    Animator anim;
 
 	// Use this for initialization
 	void Start ()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
         float move = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(move));
         rigidbody2D.velocity = new Vector2(move * maxspeed, rigidbody2D.velocity.y);
+        if(move > 0 &&!facingright)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingright)
+        {
+            Flip();
+        }
 	}
+    
 
     void Flip()
     {
+        facingright = !facingright;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
 
     }
 }
